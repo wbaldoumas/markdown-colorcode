355c75a90fdd74732bd4b1d9ee222e6ecd5356a8
Will Baldoumas <45316999+wbaldoumas@users.noreply.github.com>
2022-01-09 16:58:36 -0500

    Bootstrap Project (#2)

---
+++ src/Markdig.ColorCode/Foo.cs
@@ -0,0 +1,13 @@
+﻿namespace Markdig.ColorCode;
+
+/// <summary>
+///     Just a temporary mock class to aid in bootstrapping the library.
+/// </summary>
+public class Foo
+{
+    /// <summary>
+    ///     Just a temporary mock method to aid in bootstrapping the library.
+    /// </summary>
+    /// <returns>Bar</returns>
+    public string Bar() => "Bar";
+}
